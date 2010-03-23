3b21128703bcb184f637810371d19563168e7a60
thetoolman <thetoolman@9c53cf46-e557-11de-ab96-fd2e03e142b3>
2010-03-23 18:06:21 +0000

    added float to mimicCSS
    git-svn-id: http://ufd.googlecode.com/svn/trunk@183 9c53cf46-e557-11de-ab96-fd2e03e142b3

--- src/jquery.ui.ufd.js
+++ src/jquery.ui.ufd.js
@@ -1168,7 +1168,7 @@ $.extend($.ui.ufd, {
 		suffix: "_ufd", // suffix for pseudo-dropdown text input name attr.  
 		dropDownID: "ufd-container", // ID for a root-child node for storing dropdown lists. avoids ie6 zindex issues by being at top of tree.
 		logSelector: "#log", // selector string to write log into, if present.
-		mimicCSS: ["marginLeft","marginTop","marginRight","marginBottom"], //copy these properties to widget. Width auto-copied unless min/manual.
+		mimicCSS: ["float", "marginLeft","marginTop","marginRight","marginBottom"], //copy these properties to widget. Width auto-copied unless min/manual.
 
 		infix: true, //infix search, not prefix 
 		addEmphasis: false, // add <EM> tags around matches.
